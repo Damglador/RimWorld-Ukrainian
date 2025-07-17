e76a7821b578a3329cd89cd6076777288532d12f
Damglador <vse.stopchanskyi@gmail.com>
2025-07-18 00:34:55 +0200

    Add mod for testing

---
+++ Mod/build.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+cd $(dirname $0)
+
+COMPONENTS=("Core" "Royalty" "Ideology" "Biotech" "Anomaly" "Odyssey")
+
+for DLC in "${COMPONENTS[@]}"; do
+	LANG_DIR="$DLC/Languages/Ukrainian (Crowdin)"
+	mkdir -p "$LANG_DIR"/
+	cp -r CrowdinFiles/"$DLC"/* "$LANG_DIR"/
+	
+	for dir in Keyed DefInjected; do
+	  find "$LANG_DIR/$dir" -type f -name '*.xml' | while read -r file; do
+	  	if [[ "$file" != *_${DLC}.xml ]]; then
+	        mv "$file" "${file%.xml}_${DLC}.xml"
+	  	fi
+	  done
+	done
+done
