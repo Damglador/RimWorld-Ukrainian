67d51bc569144b8a3a6a92649179329edcdd9392
Damglador <vse.stopchanskyi@gmail.com>
2025-07-18 02:58:08 +0200

    Add script for pulling translation and updating the mod

---
+++ update.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+CROWDIN_PROJECT_ID=295257
+[ -z CROWDIN_PERSONAL_TOKEN ] && CROWDIN_PERSONAL_TOKEN=""
+
+MOD_DIR=Mod
+
+cd $(dirname $0)
+
+# Завантажити переклад з Crowdin (потрібен CROWDIN_PERSONAL_TOKEN менеджера проєкту)
+crowdin download
+
+# Дати файли моду
+mkdir -p Mod/CrowdinFiles && cp -r uk/* "$MOD_DIR"/CrowdinFiles
+
+# Наказати моду оновитися
+[ ! -x "$MOD_DIR"/build.sh ] && chmod u+x "$MOD_DIR"/build.sh
+"$MOD_DIR"/build.sh
