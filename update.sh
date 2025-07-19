5c520b1b08f561adf3ba0033d4ca0f9ac0dbab17
Damglador <vse.stopchanskyi@gmail.com>
2025-07-19 20:37:43 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -3,12 +3,10 @@
 export CROWDIN_PROJECT_ID=295257
 [ -z CROWDIN_PERSONAL_TOKEN ] && export CROWDIN_PERSONAL_TOKEN=""
 
-MOD_DIR=Mod
-
 cd $(dirname $0)
 
 # Завантажити переклад з Crowdin (потрібен CROWDIN_PERSONAL_TOKEN менеджера проєкту)
 crowdin download
 
 # Оновити мод
-cp -r uk/* "$MOD_DIR"/
+cp -r uk/* Mod/
