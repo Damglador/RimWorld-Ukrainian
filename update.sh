6a5c314d22740b0cdd1b81610a21ef92de1ed77f
Damglador <vse.stopchanskyi@gmail.com>
2025-07-21 21:12:30 +0200

    Commit translation before updating the mod

--- update.sh
+++ update.sh
@@ -14,13 +14,14 @@ crowdin download
 # Дати файли моду
 mkdir -p Mod/CrowdinFiles && cp -r uk/* "$MOD_DIR"/CrowdinFiles
 
-# Наказати моду оновитися
-[ ! -x "$MOD_DIR"/build.sh ] && chmod u+x "$MOD_DIR"/build.sh
-"$MOD_DIR"/build.sh
-
+# Commit updated translation to git
 echo "[update.sh] Committing updated translation"
 readarray -t files < <(git diff --staged --name-only)
 git restore --staged .
 git add uk/
 git commit -m "Update translation"
 [ ${#files[@]} -gt 0 ] && git add "${files[@]}" # Check if not empty avoid messages that I didn't commit anything
+
+# Наказати моду оновитися
+[ ! -x "$MOD_DIR"/build.sh ] && chmod u+x "$MOD_DIR"/build.sh
+"$MOD_DIR"/build.sh
