b70ccff641d34015d48bc0338ad947f9ea81ca8d
Damglador <vse.stopchanskyi@gmail.com>
2025-07-25 01:54:12 +0200

    Improve extract.sh
    Added option to extract multiple languages or all available languages

--- extract.sh
+++ extract.sh
@@ -4,6 +4,7 @@ cd $(dirname $0)
 
 DEST=Extract
 GAMEDIR=/home/Games/SteamLibrary/steamapps/common/RimWorld
+LANGN=() # Language name, LANG is a system variable
 
 log() {
 	printf "$*\n"
@@ -12,29 +13,42 @@ log() {
 for arg in "$@"; do
 	case "$1" in
 		--lang)
-			LANGN="$2"
+			LANGN+=("$2")
 			log "Searching for language $LANGN"
-			shift;;
+			shift 2;;
 		--gamedir)
 			GAMEDIR="$2"
 			log "Searching in game directory $GAMEDIR"
-			shift;;
+			shift 2;;
 		--dest)
 			DEST="$2"
 			log "Extracting in $DEST"
+			shift 2;;
+		--all)
+			for f in "$GAMEDIR"/Data/Core/Languages/*.tar; do
+			  name=$(basename "$f" .tar)
+			  LANGN+=("$name")
+			done
+			log "Selecting all languages from «$GAMEDIR/Data/Core/Languages»"
 			shift;;
 		--help)
 			log "Цей скрипт автоматично видобуває файли вказаної мови (окрім English) з усіх доповнень у грі. 
 
-Використання: 
-./extract.sh --lang \"Ukrainian (Українська)\" --game \"/home/Games/SteamLibrary/steamapps/common/RimWorld\"
+Використання: 	extract.sh --game ТЕКА_ГРИ МОВИ
+	або:	extract.sh --game ТЕКА_ГРИ --all
+
+Приклад:
+./extract.sh --game \"/home/Games/SteamLibrary/steamapps/common/RimWorld\" \"Ukrainian (Українська)\" \"Polish (Polski)\"
 
 Команди:
 	--help	- показати це повідомленян
 	--game	- вказати директорію гри
-	--lang	- вказати мову, яку потрібно видобути
-	--dest	- куди потрібно видобути мову";;
-		*) shift;;
+	--dest	- куди потрібно видобути мову
+	--all	- видобути всі мови"
+			shift;;
+		*)
+			LANGN+=("$1")
+			shift;;
 	esac
 done
 
@@ -54,9 +68,11 @@ if [ ! -d "$GAMEDIR/Data" ]; then
 	exit 2
 fi
 
-for dlc in "$GAMEDIR"/Data/*/; do
-	[ ! -f "$dlc"/Languages/"$LANGN.tar" ] && log "No file "$LANGN".tar in "$dlc"/Languages" && continue
-	log "Extracting files from $dlc"
-	mkdir -p "$DEST"/"$LANGN"/"$(basename "$dlc")"/
-	tar -xf "$dlc"/Languages/"$LANGN.tar" -C "$DEST"/"$LANGN"/"$(basename "$dlc")"/
+for lang in "${LANGN[@]}"; do
+	for dlc in "$GAMEDIR"/Data/*/; do
+		[ ! -f "$dlc"/Languages/"$lang.tar" ] && log "No file «$lang.tar» in "$dlc"/Languages" && continue
+		log "Extracting files from $dlc/$lang.tar"
+		mkdir -p "$DEST"/"$lang"/"$(basename "$dlc")"/
+		tar -xf "$dlc"/Languages/"$lang.tar" -C "$DEST"/"$lang"/"$(basename "$dlc")"/
+	done
 done
